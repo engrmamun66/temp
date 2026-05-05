a10f707d517f18cd1cf25f40c92b4a4368ca2429
Al-Mamun <mamun@leapinglogic.com>
2026-05-05 17:00:12 +0600

    df

--- src/services/pase-wise-pushes/push:category.ts
+++ src/services/pase-wise-pushes/push:category.ts
@@ -1,17 +1,34 @@
 import { RskRoute, EnumPageKes, RentMyPage } from '../../interfaces';
-import { pushRouteIfNotExist, prefix } from '../PushMissingRoutes';
+import { pushRouteIfNotExist, findRentmyPage, prefix } from '../PushMissingRoutes';
 
 
 export function pushCategory(routes: RskRoute[], rentmyPages: RentMyPage[], subdomain: string): void {
-    pushRouteIfNotExist(routes, {
-        title:          '{{site_name}}:: Category',
-        page_key:       EnumPageKes.products_list_by_category,
-        route_path:     '/category/:uuid',
-        content_path:   {
-            file: prefix + 'products-list.html',
-            seo_end_point: '/stores/{subdomain}/meta/category',
-        },
-        content_source: 'file',
-        _source:        'force_pushed',
-    });
+
+    const page: RentMyPage | null = findRentmyPage(rentmyPages, 'category-products');
+    if (page) {
+        pushRouteIfNotExist(routes, {
+            title:          `{{site_name}}:: ${page.name}`,
+            page_key:       EnumPageKes.products_list_by_category,
+            route_path:     '/category/:uuid',
+            content_path:  {
+                seo_end_point: '/stores/{subdomain}/meta/category',
+                api_endpoint: `pages/${page.slug}`,
+                file: '',
+            },
+            content_source: 'api',
+        }, { force_push: true });
+
+    } else {
+        pushRouteIfNotExist(routes, {
+            title:          '{{site_name}}:: Category',
+            page_key:       EnumPageKes.products_list_by_category,
+            route_path:     '/category/:uuid',
+            content_path:   {
+                file: prefix + 'products-list.html',
+                seo_end_point: '/stores/{subdomain}/meta/category',
+            },
+            content_source: 'file',
+            _source:        'force_pushed',
+        });
+    }
 }
