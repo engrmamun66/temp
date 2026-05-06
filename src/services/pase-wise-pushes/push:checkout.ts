5e843eb02e88f1821702c0322def971992cd77b2
Al-Mamun <mamun@leapinglogic.com>
2026-05-06 14:41:21 +0600

    update cOut

--- src/services/pase-wise-pushes/push:checkout.ts
+++ src/services/pase-wise-pushes/push:checkout.ts
@@ -2,32 +2,24 @@ import { RskRoute, EnumPageKes, RentMyPage } from '../../interfaces';
 import { pushRouteIfNotExist, findRentmyPage, prefix } from '../PushMissingRoutes';
 
 export function pushCheckout(routes: RskRoute[], rentmyPages: RentMyPage[], subdomain: string): void {
-    // const page = findRentmyPage(rentmyPages, 'checkout');
-    // if (page) {
-    //     pushRouteIfNotExist(routes, {
-    //         title:          `{{site_name}}:: ${page.name}`,
-    //         page_key:       EnumPageKes.checkout,
-    //         route_path:     '/' + page.slug,
-    //         content_path:   `pages/${page.slug}`,
-    //         content_source: 'api',
-    //     }, { force_push: true });
-    // } else {
-    //     pushRouteIfNotExist(routes, {
-    //         title:          '{{site_name}}:: Checkout',
-    //         page_key:       EnumPageKes.checkout,
-    //         route_path:     '/checkout',
-    //         content_path:   prefix + 'checkout.html',
-    //         content_source: 'file',
-    //         _source:        'force_pushed',
-    //     });
-    // }
+    const page = findRentmyPage(rentmyPages, 'checkout');
+    if (page) {
+        pushRouteIfNotExist(routes, {
+            title:          `{{site_name}}:: ${page.name}`,
+            page_key:       EnumPageKes.checkout,
+            route_path:     '/' + page.slug,
+            content_path:   `pages/${page.slug}`,
+            content_source: 'api',
+        }, { force_push: true });
+    } else {
+        pushRouteIfNotExist(routes, {
+            title:          '{{site_name}}:: Checkout',
+            page_key:       EnumPageKes.checkout,
+            route_path:     '/checkout',
+            content_path:   prefix + 'checkout.html',
+            content_source: 'file',
+            _source:        'force_pushed',
+        });
+    }
 
-     pushRouteIfNotExist(routes, {
-        title:          '{{site_name}}:: Checkout',
-        page_key:       EnumPageKes.checkout,
-        route_path:     '/checkout',
-        content_path:   prefix + 'checkout-2.html',
-        content_source: 'file',
-        _source:        'force_pushed',
-    });
 }
